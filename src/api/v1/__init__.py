5ac391809b59f3dbc3637ccd7ed44683c0f0f958
Atabek <atabek.atik.808@gmail.com>
2026-01-08 00:05:44 +0500

    API updated

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -1,5 +1,9 @@
 from fastapi import APIRouter
 
+from . import city
+
 routers = APIRouter(
     prefix="/v1"
 )
+
+routers.include_router(city.router)
--- src/api/v1/city.py
+++ src/api/v1/city.py
@@ -1,16 +1,31 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, status
 
-from src.database.database import get_async_session
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.exc import SQLAlchemyError
+from sqlalchemy import select
+
+from src.core import handle_exceptions
+from src.database.database import get_async_session
+from src.models import City
+from src.schemas.city import CreateCity, CityOut
 
 router = APIRouter(prefix="/city", tags=['API для создания город'])
 
-@router.post("/")
+@router.post("/", status_code=status.HTTP_201_CREATED, response_model=CityOut)
 async def add_city(
+    payload: CreateCity,
     session: AsyncSession = Depends(get_async_session)
 ):
     try:
-        pass
+        data = City(name=payload.name.strip())
+        session.add(data)
+        await session.commit()
+        return data
     except Exception as err:
-        pass
+        return await handle_exceptions(session, {}, err, "Error in City Post Endpoint")
+    
+@router.get("/", status_code=status.HTTP_200_OK, response_model=list[CityOut])
+async def get_cities_list(session: AsyncSession = Depends(get_async_session)):
+    data = (
+        await session.execute(select(City))
+    ).scalars().all()
+    return data
