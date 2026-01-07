c6cbd0f0a3d5a2c05f2ecb0b8441e0561643a10e
Atabek <atabek.atik.808@gmail.com>
2026-01-08 01:18:29 +0500

    Connection v2 endpoints

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from src.api.v1 import routers as v1_routers
+from src.api.v2 import routers as v2_routers
 
 routers = APIRouter()
 
 routers.include_router(v1_routers)
+routers.include_router(v2_routers)
