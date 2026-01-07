536b8316cd5c7d10a8496cb0b5b4d1800bb253f4
Atabek <atabek.atik.808@gmail.com>
2026-01-08 01:17:10 +0500

    Connection auth endpoints

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 
 from . import city
+from . import auth
 
 routers = APIRouter(
     prefix="/v1"
 )
 
+routers.include_router(auth.routers)
 routers.include_router(city.router)
